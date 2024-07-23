c0679fb33d7c4c925deeb7b236cade278f6122be
LeoAcevedo87 <acevedoleonardo2000@gmail.com>
2024-07-22 22:53:46 -0300

    creacion de relaciones en clase venta

--- src/main/java/com/leoacevedo/proyectofinal/model/Venta.java
+++ src/main/java/com/leoacevedo/proyectofinal/model/Venta.java
@@ -1,39 +1,44 @@
 package com.leoacevedo.proyectofinal.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Entity
-@Getter @Setter 
-public class Venta { //cada venta posee una lista de productos y uno y solo un cliente asociado
-    
+@Getter
+@Setter
+public class Venta { //cada venta posee una lista de productos, y uno y solo un cliente asociado
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo_venta;
-    private LocalDate fechaVenta;
-    
-    private Double total; 
-    
-    @OneToMany //1 venta a M productos
-    @JoinColumn(name = "venta_id", referencedColumnName = "codigo_venta") 
-    //La columna venta_id (tabla Producto) referirá a la columna codigo_venta (tabla Venta)
+    private LocalDate fecha_venta;
+    private Double total;
+
+    @ManyToMany
+    @JoinTable(
+            name = "venta_producto", //nombre tabla intermedia 
+            joinColumns = @JoinColumn(name = "cod_venta", //columnas que tendra: hace relacion a la clase venta o a la clave primaria de venta
+                    referencedColumnName = "codigo_venta"), //va a referenciar a la columna de ventas "codigo_venta" 
+
+            inverseJoinColumns = @JoinColumn(name = "cod_producto", //columnas que tendra: hace relacion a la clase venta o a la clave primaria de venta
+                    referencedColumnName = "codigo_producto") //debemos poner el nombre del atributo primario de la clase producto
+    )
     private List<Producto> listaProductos;
-    
-    @ManyToOne //M ventas a 1 cliente
-    @JoinColumn(name = "id_cliente", referencedColumnName = "id_cliente")
-    //La columna id_cliente (tabla Venta) referirá a la columna id_cliente (tabla Cliente)
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente unCliente;
 
     public Venta() {
@@ -41,11 +46,10 @@ public class Venta { //cada venta posee una lista de productos y uno y solo un c
 
     public Venta(Long codigoVenta, LocalDate fechaVenta, Double total, List<Producto> listaProductos, Cliente unCliente) {
         this.codigo_venta = codigoVenta;
-        this.fechaVenta = fechaVenta;
+        this.fecha_venta = fechaVenta;
         this.total = total;
         this.listaProductos = listaProductos;
         this.unCliente = unCliente;
     }
-    
-    
+
 }
